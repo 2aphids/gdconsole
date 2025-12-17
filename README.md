668c73f502567c959d0f8f50c5f2f00020269839
aphd <aphidarium@proton.me>
2025-12-17 00:29:49 +0000

    basic working version

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 
 To use this plugin, you must:
 
-- instantiate the scene located at `res://addons/gdconsole/console.tscn` in your main scene, and
-- place your global variables in an autoloaded script named `Variables`.
+- instantiate the scene located at `res://addons/gdconsole/console.tscn` in your main scene,
+- place your global variables in an autoloaded script named `Variables`, and
+- create a new input named "console", with a key of your choice.
--- scripts/console.gd
+++ scripts/console.gd
@@ -3,21 +3,21 @@ extends BoxContainer
 @onready var output: VBoxContainer = $Output
 @onready var line_edit: LineEdit = $Input/LineEdit
 
-const SCENE_DIR := "scenes/maps/"
+# const SCENE_DIR := "scenes/maps/"
 
 
-func _cmd_prop(args: Array) -> void:
-	if (args == [] or !get_node("/root/Main/World")):
-		return
-
-	print("Spawning prop `%s`" % args[0])
-
-	var world: Node3D = get_node("/root/Main/World")
-	var res := load("res://models/gltf/props/%s.glb" % args[0])
-	if (res):
-		var prop: PhysicsProp = res.instantiate()
-		prop.global_position = Variables.player.find_child("HeldItemPoint", true, false).global_position
-		world.add_child(prop)
+# func _cmd_prop(args: Array) -> void:
+# 	if (args == [] or !get_node("/root/Main/World")):
+# 		return
+#
+# 	print("Spawning prop `%s`" % args[0])
+#
+# 	var world: Node3D = get_node("/root/Main/World")
+# 	var res := load("res://models/gltf/props/%s.glb" % args[0])
+# 	if (res):
+# 		var prop: PhysicsProp = res.instantiate()
+# 		prop.global_position = Variables.player.find_child("HeldItemPoint", true, false).global_position
+# 		world.add_child(prop)
 
 
 func _cmd_print(args: Array) -> void:
@@ -27,19 +27,19 @@ func _cmd_print(args: Array) -> void:
 	print(result)
 
 
-func _cmd_map(args: Array) -> void:
-	if (args == []):
-		print("Available maps:")
-		var dir = DirAccess.open(SCENE_DIR)
-		assert(dir, "Scene directory invalid")
-		dir.list_dir_begin()
-		var file_name = dir.get_next()
-		while file_name != "":
-			if dir.current_is_dir():
-				print("> " + file_name)
-			file_name = dir.get_next()
-		return
-	get_node("/root/Main").load_map(SCENE_DIR + args[0] + ".tscn")
+# func _cmd_map(args: Array) -> void:
+# 	if (args == []):
+# 		print("Available maps:")
+# 		var dir = DirAccess.open(SCENE_DIR)
+# 		assert(dir, "Scene directory invalid")
+# 		dir.list_dir_begin()
+# 		var file_name = dir.get_next()
+# 		while file_name != "":
+# 			if dir.current_is_dir():
+# 				print("> " + file_name)
+# 			file_name = dir.get_next()
+# 		return
+# 	get_node("/root/Main").load_map(SCENE_DIR + args[0] + ".tscn")
 
 
 func toggle() -> void:
@@ -61,6 +61,9 @@ func _ready() -> void:
 
 
 func _input(_event) -> void:
+	if Input.is_action_just_pressed("console", false):
+		toggle()
+
 	if (!line_edit.visible):
 		return
 
@@ -77,24 +80,24 @@ func _submit() -> void:
 
 	if (cmd.is_valid()):
 		cmd.call(cmds.slice(1, cmds.size()))
-	elif (Variables.get(cmds[0]) != null):
-		var args = cmds.slice(0, cmds.size())
-		if (args.size() < 1):
-			return
-		elif (args.size() > 1):
-			var t = typeof(Variables.get(args[0]))
-			match t:
-				TYPE_STRING:
-					Variables.set(args[0], args[1])
-				TYPE_BOOL:
-					Variables.set(args[0], true if args[1] == "true" else false)
-				TYPE_INT:
-					Variables.set(args[0], args[1].to_int())
-				TYPE_FLOAT:
-					Variables.set(args[0], args[1].to_float())
-		if Variables.MAN.has(str(args[0])):
-			print(Variables.MAN[str(args[0])])
-		print(str(args[0]) + " = " + str(Variables.get(args[0])))
+	# elif (Variables.get(cmds[0]) != null):
+	# 	var args = cmds.slice(0, cmds.size())
+	# 	if (args.size() < 1):
+	# 		return
+	# 	elif (args.size() > 1):
+	# 		var t = typeof(Variables.get(args[0]))
+	# 		match t:
+	# 			TYPE_STRING:
+	# 				Variables.set(args[0], args[1])
+	# 			TYPE_BOOL:
+	# 				Variables.set(args[0], true if args[1] == "true" else false)
+	# 			TYPE_INT:
+	# 				Variables.set(args[0], args[1].to_int())
+	# 			TYPE_FLOAT:
+	# 				Variables.set(args[0], args[1].to_float())
+	# 	if Variables.MAN.has(str(args[0])):
+	# 		print(Variables.MAN[str(args[0])])
+	# 	print(str(args[0]) + " = " + str(Variables.get(args[0])))
 	else:
 		print("Command or variable '" + cmds[0] + "' does not exist.")
 
@@ -117,9 +120,9 @@ func _text_changed(_new_text: String) -> void:
 	if (line_edit.text.length() == 0):
 		return
 
-	for prop in Variables.get_property_list():
-		if (prop['name'].begins_with(line_edit.text)):
-			var label: Label = Label.new()
-			label.text = prop['name']
-			line_edit.current_suggestions.push_back(prop['name'])
-			$Input.add_child(label)
+	# for prop in Variables.get_property_list():
+	# 	if (prop['name'].begins_with(line_edit.text)):
+	# 		var label: Label = Label.new()
+	# 		label.text = prop['name']
+	# 		line_edit.current_suggestions.push_back(prop['name'])
+	# 		$Input.add_child(label)
